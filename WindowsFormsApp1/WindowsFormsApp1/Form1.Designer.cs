c4d0dd494e489bdeb75363cb173bcf6d305b4308
zyqcosta10 <zyqcosta1098741236>
2019-10-24 12:40:45 +0800

    初版，需进一步调细节

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,24 +42,51 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(1257, 674);
+            this.dataGridView1.Size = new System.Drawing.Size(1028, 601);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 693);
+            this.button1.Location = new System.Drawing.Point(23, 637);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "读取Excel";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(137, 637);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(596, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 684);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "导出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(137, 684);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(596, 23);
+            this.progressBar2.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1281, 745);
+            this.ClientSize = new System.Drawing.Size(1049, 745);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -71,6 +101,9 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar2;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,13 +20,13 @@ namespace WindowsFormsApp1
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            DataGet();
+            //DataGet();
         }
 
         private void DataGet()
         {
-            string sql = "select * from CheckWork";
-            dataGridView1.DataSource = MyClass.dataBaseClass.OperateDataSet(sql).Tables[0].DefaultView;
+            //string sql = "select * from CheckWork";
+            //dataGridView1.DataSource = MyClass.dataBaseClass.OperateDataSet(sql).Tables[0].DefaultView;
         }
 
         /// <summary>
@@ -36,29 +36,26 @@ namespace WindowsFormsApp1
         private void ReadExcel(string FilePath)
         {
             // ExcelOperate EO = new ExcelOperate();
-            Excel.Application myExcel = new Excel.Application();
+            //Excel.Application myExcel = new Excel.Application();
 
-            //取得Excel文件中共有的sheet的数目
+            ////取得Excel文件中共有的sheet的数目
 
-            object oMissing = System.Reflection.Missing.Value;
+            //object oMissing = System.Reflection.Missing.Value;
 
-            myExcel.Application.Workbooks.Open(FilePath, oMissing, oMissing, oMissing, oMissing, oMissing,
-            oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing);
+            //myExcel.Application.Workbooks.Open(FilePath, oMissing, oMissing, oMissing, oMissing, oMissing,
+            //oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing);
 
-            Excel.Workbook myBook = myExcel.Workbooks[1];
+            //Excel.Workbook myBook = myExcel.Workbooks[1];
             //int sheetNum = myBook.Worksheets.Count;
 
             ExcelOpreate ExcelOp = new ExcelOpreate(FilePath);
             DataTable tblDatas = new DataTable("Datas");
             DataColumn dc = null;
-
-
-
-            dc = tblDatas.Columns.Add("样品号", Type.GetType("System.String"));
-            dc = tblDatas.Columns.Add("井深", Type.GetType("System.String"));
-            dc = tblDatas.Columns.Add("空隙度", Type.GetType("System.String"));
-            dc = tblDatas.Columns.Add("渗透率水平", Type.GetType("System.String"));
-            dc = tblDatas.Columns.Add("渗透率垂直", Type.GetType("System.String"));
+            dc = tblDatas.Columns.Add("姓名", Type.GetType("System.String"));
+            dc = tblDatas.Columns.Add("部门", Type.GetType("System.String"));
+            dc = tblDatas.Columns.Add("日期", Type.GetType("System.String"));
+            dc = tblDatas.Columns.Add("状态", Type.GetType("System.String"));
+            dc = tblDatas.Columns.Add("考勤代码", Type.GetType("System.String"));
             //for (int j = 1; j < sheetNum; j++)
             //{
             ExcelOp.SheelTableIndex = 1;
@@ -68,40 +65,140 @@ namespace WindowsFormsApp1
             //{
             for (int i = 5; i < 200; i++)
             {
-                //if (!ExcelOp.ReadExcelCellString(i, 2).Contains("分析人") && ExcelOp.ReadExcelCellString(i, 2) != "")
-                //{
-                newRow = tblDatas.NewRow();
-                newRow["样品号"] = ExcelOp.ReadExcelCellString(i, 1);
-                newRow["井深"] = ExcelOp.ReadExcelCellString(i, 2);
-                newRow["空隙度"] = ExcelOp.ReadExcelCellString(i, 7);
-                newRow["渗透率水平"] = ExcelOp.ReadExcelCellString(i, 8);
-                newRow["渗透率垂直"] = ExcelOp.ReadExcelCellString(i, 9);
-
-                tblDatas.Rows.Add(newRow);
-                //}
-                //else
-                //{
-                //    break;
-                //}
+                progressBar1.Maximum = 100;
+                if (ExcelOp.ReadExcelCellString(i, 1) != "")
+                {
+                    for (int j = 23; j < 54; j++)
+                    {
+                        newRow = tblDatas.NewRow();
+                        newRow["姓名"] = ExcelOp.ReadExcelCellString(i, 1);
+                        newRow["部门"] = ExcelOp.ReadExcelCellString(i, 2);
+                        newRow["日期"] = ExcelOp.ReadExcelCellString(3, j);
+                        newRow["状态"] = ExcelOp.ReadExcelCellString(i, j);
+                        newRow["考勤代码"] = GetWorkCode(ExcelOp.ReadExcelCellString(i, j));
+                        //newRow["渗透率水平"] = ExcelOp.ReadExcelCellString(i, 8);
+                        //newRow["渗透率垂直"] = ExcelOp.ReadExcelCellString(i, 9);
+                        tblDatas.Rows.Add(newRow);
+                    }
+                    progressBar1.Value = i + 1;
+                }
+                else
+                {
+                    break;
+                }
+                progressBar1.Value = 100;
             }
             //}
             //progressBar1.Maximum = sheetNum;
             //progressBar1.Value = j + 1;
-            //}
-
+            //}           
             dataGridView1.DataSource = tblDatas;
             GetTB = tblDatas;
-
-            MessageBox.Show("共" + tblDatas.Rows.Count.ToString() + "行");
+            //MessageBox.Show("共" + tblDatas.Rows.Count.ToString() + "行");
             ExcelOp.KillExcel();
         }
 
 
-
+        private string GetWorkCode(string WorkState)
+        {
+            if (WorkState == "正常")
+            {
+                return "P";
+            }
+            else if (WorkState == "休息")
+            {
+                return "O";
+            }
+            else if (WorkState == "病假")
+            {
+                return "S";
+            }
+            else if (WorkState == "年假")
+            {
+                return "A";
+            }
+            else if (WorkState == "旷工")
+            {
+                return "X";
+            }
+            else if (WorkState == "事假")
+            {
+                return "E";
+            }
+            else if (WorkState == "早退")
+            {
+                return "LF";
+            }
+            else if (WorkState == "辞职")
+            {
+                return "R";
+            }
+            else if (WorkState == "加班")
+            {
+                return "OT";
+            }
+            else if (WorkState == "无薪年假")
+            {
+                return "UP";
+            }
+            else if (WorkState == "补假")
+            {
+                return "L";
+            }
+            else if (WorkState == "迟到")
+            {
+                return "LR";
+            }
+            else if (WorkState == "产假")
+            {
+                return "Y";
+            }
+            else if (WorkState == "丧假")
+            {
+                return "F";
+            }
+            else if (WorkState == "公出")
+            {
+                return "B";
+            }
+            else if (WorkState == "婚假")
+            {
+                return "M";
+            }
+            else if (WorkState == "法定假日")
+            {
+                return "PH";
+            }
+            else
+            {
+                return WorkState;
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             ReadExcel("E:\\TEST\\1.xlsx");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExcelOpreate ExcelOp = new ExcelOpreate("E:\\TEST\\2.xlsx");
+            int count = 0;
+            progressBar2.Maximum = 50;
+            for (int i = 7; i < 45; i++)
+            {
+                ExcelOp.WriteExcelCell(i, 2, GetTB.Rows[count]["姓名"].ToString());
+                for (int j = 0; j < 31; j++)
+                {
+                    ExcelOp.WriteExcelCell(i, j + 3, GetTB.Rows[count]["考勤代码"].ToString());
+                    count++;
+                }
+                progressBar2.Value = i + 1;
+            }
+            progressBar2.Value = 50;
+            ExcelOp.Save();
+            ExcelOp.KillExcel();
+
         }
     }
 }
